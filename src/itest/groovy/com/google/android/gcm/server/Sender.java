3d2f462663753929e4ce7822b8d587dbaa5ec681
tolis-e <tolisemm@yahoo.gr>
2013-07-26 16:08:18 +0200

    Fix licences to APNs and GCM mock classes

--- src/itest/groovy/com/google/android/gcm/server/Sender.java
+++ src/itest/groovy/com/google/android/gcm/server/Sender.java
@@ -1,16 +1,15 @@
-/**
- * JBoss, Home of Professional Open Source
- * Copyright Red Hat, Inc., and individual contributors.
+/*
+ * Copyright 2012 Google Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
@@ -24,6 +23,11 @@ import javax.enterprise.context.ApplicationScoped;
 
 import org.jboss.aerogear.connectivity.common.SenderSimulation;
 
+/**
+ * 
+ * This class mocks the original com.google.android.gcm.server.Sender class and is used for testing reasons. 
+ *
+ */
 @SenderSimulation
 @ApplicationScoped
 public class Sender {
